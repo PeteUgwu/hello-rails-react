593d8bea514ecd12862103704a5352652e30ca95
peter <peterugwu64@gmail.com>
2023-05-17 01:15:57 +0100

    Create static page, API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    get 'random_greeting', to: 'greetings#random_greeting'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello, how are you?",
+  "Hey there! Welcome to our platform. We're glad you're here!",
+  "Warm greetings to you!",
+  "Hi there! We're excited to have you here.",
+  "I hope you have a fantastic day ahead."
+]
+
+greetings.each do |content|
+  Message.create!(content: content)
+end
